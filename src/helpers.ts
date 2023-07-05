e62bcff8b28dbac7124771358a80691985585f25
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-07-05 09:48:51 -0400

    merge these two together

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,4 @@
 import { walk } from 'svelte/compiler';
-import { handleActionNode } from './Action';
 import { traverse } from './traverse';
 
 import type { Config, Node } from './types';
@@ -90,4 +89,70 @@ export function traverseBlock(args: BlockArgs) {
 			this.skip();
 		},
 	});
+}
+
+type HandleActionNodeArgs = {
+	blockNode: TemplateNode;
+	knownIdentifiers: Set<string>;
+	actionNode: TemplateNode;
+	leftOverActions: TemplateNode[];
+	config: Config;
+};
+
+/**
+ * D
+ */
+function handleActionNode({
+	config,
+	actionNode,
+	leftOverActions,
+	knownIdentifiers,
+	blockNode,
+}: HandleActionNodeArgs) {
+	const expression = actionNode.expression as Node;
+	const expressionIdentifiers = new Set<string>();
+	let inserted = false;
+
+	// any other expression type...
+	// i.e. use:melt={$builder({ arg1: '', arg2: '' })}
+	if (expression.type !== 'Identifier') {
+		const expressionContent = config.content.substring(expression.start, expression.end);
+		extractIdentifiers(expression).forEach((identifier) =>
+			expressionIdentifiers.add(identifier)
+		);
+
+		// const
+		for (const identifier of expressionIdentifiers) {
+			if (!knownIdentifiers.has(identifier)) continue;
+
+			// make this into a {@const} block
+			const start = blockNode.children?.at(0)?.start;
+			const constIdentifier = getMeltBuilderName(config.builderCount++);
+			if (!start) throw Error('This is unreachable');
+
+			config.builders.push({
+				identifierName: constIdentifier,
+				startPos: actionNode.start,
+				endPos: actionNode.end,
+			});
+
+			config.markup.prependRight(
+				start,
+				`{@const ${constIdentifier} = ${expressionContent}}`
+			);
+			inserted = true;
+			break;
+		}
+
+		if (inserted === false) {
+			// add it to the list of untouched actions
+			leftOverActions.push(actionNode);
+		}
+	} else {
+		config.builders.push({
+			identifierName: expression.name,
+			startPos: actionNode.start,
+			endPos: actionNode.end,
+		});
+	}
 }
