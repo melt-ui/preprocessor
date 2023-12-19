f3116840e554f7a31b8b8be99d5ca3b74ffc5cf8
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-19 12:27:19 -0500

    feat: Added built in `sequence`

---
+++ src/sequence.ts
@@ -0,0 +1,32 @@
+import { preprocess } from 'svelte/compiler';
+import { PreprocessorGroup, Processed } from 'svelte/types/compiler/preprocess';
+
+export function sequence(preprocessors: PreprocessorGroup[]): PreprocessorGroup {
+	return {
+		async markup({ content, filename }): Promise<Processed> {
+			const dependencies = [];
+			let code = content;
+			let map: Processed['map'];
+			let attributes: Processed['attributes'];
+			let toString: Processed['toString'];
+
+			for (const pp of preprocessors) {
+				const processed = await preprocess(code, pp, { filename });
+				if (processed && processed.dependencies) {
+					dependencies.push(...processed.dependencies);
+				}
+				code = processed ? processed.code : code;
+				map = processed.map ?? map;
+				attributes = processed.attributes ?? attributes;
+				toString = processed.toString ?? toString;
+			}
+
+			return {
+				code,
+				dependencies,
+				map,
+				attributes,
+			};
+		},
+	};
+}
