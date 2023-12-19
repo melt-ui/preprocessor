071671c1a37dc43964e3cb9b35f6666698ad6dac
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2023-12-19 12:42:20 -0500

    jsdoc example

--- src/sequence.ts
+++ src/sequence.ts
@@ -3,6 +3,24 @@
 import { preprocess } from 'svelte/compiler';
 import { PreprocessorGroup, Processed } from 'svelte/types/compiler/preprocess';
 
+/**
+ * A Svelte preprocessor that wraps other preprocessors and forces them to run sequentially.
+ *
+ * @example
+ * ```js
+ * // svelte.config.js
+ * import { preprocessMeltUI, sequence } from '@melt-ui/pp';
+ *
+ * const config = {
+ * 	// ... other svelte config options
+ * 	preprocess: sequence([
+ * 		// ... other preprocessors (e.g. `vitePreprocess()`)
+ * 		preprocessMeltUI()
+ * 	])
+ * 	// ...
+ * };
+ * ```
+ */
 export function sequence(preprocessors: PreprocessorGroup[]): PreprocessorGroup {
 	return {
 		async markup({ content, filename }): Promise<Processed> {
